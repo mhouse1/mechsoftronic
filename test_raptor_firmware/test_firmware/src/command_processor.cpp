e43f3984614eb037f425ceff9f6f458c9192febf
Dynames <patzer9@gmail.com>
2015-04-28 22:39:55 -0400

    added payload manipulation and execution functions to be called

--- test_raptor_firmware/test_firmware/src/command_processor.cpp
+++ test_raptor_firmware/test_firmware/src/command_processor.cpp
@@ -18,25 +18,178 @@ CommandProcessor::CommandProcessor()
 }
 
 
+/////////////////////////////////////////////////////////////////////////////
+///@brief	converts a string of ascii characters into 32 bits
+///
+///@details	For example: an ascii string with character sequence
+///			ascii(128),ascii(52),ascii(21),ascii(7)
+///			will return a u32 10000000001101000001010100000111
+///			index selects which 4 bytes is converted to u32, for example
+///			setting index = 0 will return the first four bytes in a string
+///			setting index = 1 will return the second four bytes in a string
+/////////////////////////////////////////////////////////////////////////////
+alt_u32 CommandProcessor::get_long_from_string(string in_string, alt_u8 index)
+{
+	alt_u8 start = index * 4;
+	alt_u8 stop  = start + 4;
+	alt_u32 data;
+	alt_u32 temp;
+	for(int i = start; i < stop; ++i)
+	{
+		temp = int((unsigned char)in_string[i]);
+		data =  data << 8;
+		data = data | temp;
+		//cout<<"data "<<data<<" temp "<<temp<<endl;
+	}
+	return data;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///@brief takes string payload and converts to struct of dir and step count
+/////////////////////////////////////////////////////////////////////////////
+cnc_stepdir CommandProcessor::get_step_and_dir(string payload)
+{
+	cnc_stepdir stepdir;
+	alt_u32 value;
+	value = get_long_from_string(payload,0);
+	//cout<<"value = "<<value<<endl;
+	stepdir.data.ULONG = value;
+	return stepdir;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///@brief takes u32 value and converts to struct of dir and step count
+/////////////////////////////////////////////////////////////////////////////
+cnc_stepdir CommandProcessor::get_step_and_dir(alt_u32 value)
+{
+	cnc_stepdir stepdir;
+	//cout<<"value = "<<value<<endl;
+	stepdir.data.ULONG = value;
+	return stepdir;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///@brief 	jog the axis in the direction and number of steps specified in
+///			the payload.
+/////////////////////////////////////////////////////////////////////////////
+void CommandProcessor::jog_z(string payload)
+{
+	cnc_stepdir stepdiraxis;
+	stepdiraxis = get_step_and_dir(payload);
+	this->CNC_CONTROL.CTRL.CTRL_BITS.DirectionZ = stepdiraxis.data.bits.dir?up:down;
+	this->StepNumZ = stepdiraxis.data.bits.step;
+	this->MoveZ();
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///@brief 	jog the axis in the direction and number of steps specified in
+///			the payload.
+/////////////////////////////////////////////////////////////////////////////
+void CommandProcessor::jog_y(string payload)
+{
+	cnc_stepdir stepdiraxis;
+	stepdiraxis = get_step_and_dir(payload);
+	this->CNC_CONTROL.CTRL.CTRL_BITS.DirectionY = stepdiraxis.data.bits.dir?up:down;
+	this->StepNumY = stepdiraxis.data.bits.step;
+	this->MoveY();
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///@brief 	jog the axis in the direction and number of steps specified in
+///			the payload.
+/////////////////////////////////////////////////////////////////////////////
+void CommandProcessor::jog_x(string payload)
+{
+	cnc_stepdir stepdiraxis;
+	stepdiraxis = get_step_and_dir(payload);
+	this->CNC_CONTROL.CTRL.CTRL_BITS.DirectionX = stepdiraxis.data.bits.dir?up:down;
+	this->StepNumX = stepdiraxis.data.bits.step;
+	this->MoveX();
+}
+/////////////////////////////////////////////////////////////////////////////
+///@brief 	jog the axes in the direction and number of steps specified in
+///			the payload.
+///@details	the first four bytes of the string contains stepdir for x axis
+///			the next four bytes of the string contains stepdir for y axis
+/////////////////////////////////////////////////////////////////////////////
+void CommandProcessor::jog_xy(string payload)
+{
+	alt_u32 valuex = get_long_from_string(payload,0);
+	alt_u32 valuey = get_long_from_string(payload,1);
+	cnc_stepdir stepdirx;
+	cnc_stepdir stepdiry;
+	stepdirx = get_step_and_dir(valuex);
+	stepdiry = get_step_and_dir(valuey);
+	this->CNC_CONTROL.CTRL.CTRL_BITS.DirectionX = stepdirx.data.bits.dir?up:down;
+	this->CNC_CONTROL.CTRL.CTRL_BITS.DirectionY = stepdiry.data.bits.dir?up:down;
+	this->StepNumX = stepdirx.data.bits.step;
+	this->StepNumY = stepdiry.data.bits.step;
+	this->MoveXY();
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///@brief 	set the counts for pulse high and low
+/////////////////////////////////////////////////////////////////////////////
+void CommandProcessor::set_pw_z(string payload)
+{
+	alt_u32 valueh = get_long_from_string(payload,0);
+	alt_u32 valuel = get_long_from_string(payload,1);
+	this->WritePulseInfoZ(valueh,valuel);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///@brief 	set the counts for pulse high and low
+/////////////////////////////////////////////////////////////////////////////
+void CommandProcessor::set_pw_y(string payload)
+{
+	alt_u32 valueh = get_long_from_string(payload,0);
+	alt_u32 valuel = get_long_from_string(payload,1);
+	this->WritePulseInfoY(valueh,valuel);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///@brief 	set the counts for pulse high and low
+/////////////////////////////////////////////////////////////////////////////
+void CommandProcessor::set_pw_x(string payload)
+{
+	alt_u32 valueh = get_long_from_string(payload,0);
+	alt_u32 valuel = get_long_from_string(payload,1);
+	this->WritePulseInfoX(valueh,valuel);
+}
+
+/////////////////////////////////////////////////////////////////////////////
+///@brief 	process command, given command number and payload string
+/////////////////////////////////////////////////////////////////////////////
 int CommandProcessor::input_command(alt_u8 command, string payload)
 {
-	cout<<"command "<<command<<" payload "<<payload<<endl;
+
+	//cout<<"command "<<command<<" payload "<<payload<<" payload-int "<<int((unsigned char)payload[0])<<endl;
 	switch (command)
 	{
 	case(JOG_Z):
-			cout<<"JOG_Z"<<endl;
+		this->jog_z(payload);
 		break;
 	case(JOG_Y):
-			cout<<"JOG_Y"<<endl;
+		this->jog_y(payload);
 		break;
 	case(JOG_X):
-			cout<<"JOG_X"<<endl;
+		this->jog_x(payload);
 		break;
 	case(JOG_XY):
-			cout<<"JOG_XY"<<endl;
+		this->jog_xy(payload);
+		break;
+	case(SET_PW_Z):
+		this->set_pw_z(payload);
+		break;
+	case(SET_PW_Y):
+		this->set_pw_y(payload);
+		break;
+	case(SET_PW_X):
+		this->set_pw_x(payload);
 		break;
 	default:
 		cout<<"unrecognized command received"<<endl;
-		break;
+		return 1;
 	}
+	return 0;
 }
--- test_raptor_firmware/test_firmware/src/command_processor.hpp
+++ test_raptor_firmware/test_firmware/src/command_processor.hpp
@@ -17,6 +17,7 @@
 #include <iostream>
 
 #include "types.hpp"
+#include "cncmachine.hpp"
 
 using namespace std;
 
@@ -25,13 +26,40 @@ enum possible_commands {JOG_Z = 0, 		JOG_Y, 		JOG_X, 	 	JOG_XY,
 							     	 	START, 		PAUSE, 		CANCEL
 						};
 
-class CommandProcessor
+struct cnc_stepdir
+{
+	union
+	{
+		alt_u32 ULONG;
+		struct
+		{
+			alt_u32 step : 31;
+			alt_u32 dir : 1;
+		}bits;
+	}data;
+};
+
+class CommandProcessor : private CncMachine
 {
 public:
 	CommandProcessor();
 
 	possible_commands selected_command;
 	int input_command(alt_u8 command, string payload);
+
+private:
+	alt_u32 get_long_from_string(string in_string, alt_u8 index);
+	list<string> get_fields(string in_string);
+	cnc_stepdir get_step_and_dir(string payload);
+	cnc_stepdir get_step_and_dir(alt_u32 value);
+
+	void jog_z(string payload);
+	void jog_y(string payload);
+	void jog_x(string payload);
+	void jog_xy(string payload);
+	void set_pw_z(string payload);
+	void set_pw_y(string payload);
+	void set_pw_x(string payload);
 };
 
 
