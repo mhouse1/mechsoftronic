a01a1b734aa58ff2c2674d0763fe8c43404f4547
Dynames <patzer9@gmail.com>
2015-04-28 22:41:17 -0400

    removed iostream stdout used for debugging

--- test_raptor_firmware/test_firmware/src/communication_tcp_based.cpp
+++ test_raptor_firmware/test_firmware/src/communication_tcp_based.cpp
@@ -31,30 +31,26 @@ int CommSimple::input(UBYTE new_byte)
 	switch(this->decode_state)
 	{
 	case(WAIT_COMMAND):
-		cout<<"command received char: "<<new_byte<<" received int: "<<static_cast<int>(new_byte)<<endl;
+		//cout<<"command received char: "<<new_byte<<" received int: "<<static_cast<int>(new_byte)<<endl;
 		this->msg_command = static_cast<int>(new_byte);
 		this->decode_state = WAIT_LENGTH;
 		this->msg_received_index = 0;
+		this->msg_payload = "";
 		return this->decode_status = RECEIVED_COMMAND;
 	case(WAIT_LENGTH):
-		cout<<"length received char: "<<new_byte<<" received int: "<<static_cast<int>(new_byte)<<endl;
 		this->msg_length = static_cast<int>(new_byte);
 		this->decode_state = WAIT_MSG;
 		return this->decode_status = RECEIVED_LENGTH;
 	case(WAIT_MSG):
 		this->msg_received_index += 1;
-		cout<<"msg received "<<new_byte<<" index "<<this->msg_received_index<<endl;
-
 		this->msg_payload += new_byte;
 		if (this->msg_received_index < this->msg_length)
 		{
-
-
 			return this->decode_status = BUILDING_MSG;
 		}
 		else //full message received, process message then go to next state
 		{
-			//process_message(command,payload);
+			//process the command with payload
 			this->input_command(this->msg_command,this->msg_payload);
 			this->decode_state = WAIT_COMMAND;
 			return this->decode_status = DONE;
